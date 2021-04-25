8b699d4114d96047e0aa4ae548b28a942708bf07
aliyacoub <aliyacoub1996@gmail.com>
2021-04-25 04:30:59 +0300

    home page

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './scss/style.scss';
+import './css/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/js/all.min';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const path = require('path');
+const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+  (module.exports = {
+    entry: {
+      app: './src/index.js',
+    },
+
+    output: {
+      path: path.join(__dirname, '/dist'),
+      publicPath:'',
+      filename: 'main.js',
+    },
+    mode: 'development',
+    devServer: {
+      contentBase:  path.join(__dirname, '/dist'),
+      port:1239,
+      writeToDisk: true,
+      open:true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                minimize: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader, 
+              options: {
+                publicPath: '../' 
+              }
+            },
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [
+           
+            {
+              loader:"file-loader",
+              options:{
+                name:'[name].[ext]',
+                outputPath:"images",
+            }
+            }
+          ]
+        },
+        {
+          test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: [
+          {
+            loader: "file-loader", 
+            options: {
+              name: '[name].[ext]',
+              outputPath: "fonts",
+              esModule: false,
+            }
+            }
+          ]
+        },
+        {
+          test: /\.(mov|mp4)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath:"videos",
+              }  
+            }
+          ]
+        },
+        {
+          test: require.resolve("jquery"),
+          loader: "expose-loader",
+          options: {
+            exposes: ["$", "jQuery"],
+          },
+        },
+      ],
+    },
+  
+    plugins: [
+      new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html',
+      }),
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery'",
+        "window.$": "jquery"
+      }),
+      new  OptimizeCssAssetsPlugin({}),
+    ],
+
+  });
+ 
